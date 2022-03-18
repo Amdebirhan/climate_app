babac53cf5b5d7bbcd40e873293fcb8c125be653
Amdebirhan <amdebirhanteshome@gmail.com>
2022-03-18 04:43:43 -0700

    first_commit

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -3,12 +3,12 @@ import 'dart:convert';
 
 class NetworkHelper {
   NetworkHelper(this.url);
-  final String? url;
+  final String url;
 
   //we use future becouse the method can tak an pridictable time to complete so it return a future and it will asign the value after
   //gets the data from the internet
   Future GetData() async {
-    http.Response response = await http.get(url!);
+    http.Response response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       String data = response.body;
